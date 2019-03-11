0b9f095e549d02d82a825b1b56e5fed326d5feeb
Matthew Sherborne <msherborne@gmail.com>
2019-03-11 10:23:25 +1000

    Now can parse log entries

--- src/day4.rs
+++ src/day4.rs
@@ -7,6 +7,7 @@ use std::str::FromStr;
 /// [1518-11-01 00:05] falls asleep
 /// [1518-11-01 00:25] wakes up
 
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 struct Minute {
     y: usize,
     m: usize,
@@ -26,21 +27,21 @@ impl FromStr for Minute {
             .split_whitespace()
             .collect();
         assert_eq!(parts.len(), 2);
-        let (date, time) = (parts[0], parts[1]);
-        let date = date
+        let (minute, time) = (parts[0], parts[1]);
+        let minute = minute
             .split('-')
             .map(|part| part.parse::<usize>())
             .collect::<Result<Vec<usize>, Self::Err>>()?;
-        assert_eq!(date.len(), 3);
+        assert_eq!(minute.len(), 3);
         let time = time
             .split(':')
             .map(|part| part.parse::<usize>())
             .collect::<Result<Vec<usize>, Self::Err>>()?;
         assert_eq!(time.len(), 2);
         Ok(Minute {
-            y: date[0],
-            m: date[1],
-            d: date[2],
+            y: minute[0],
+            m: minute[1],
+            d: minute[2],
             h: time[0],
             n: time[1],
         })
@@ -59,8 +60,8 @@ fn test_minute_from_str() {
 }
 
 /// The different kinds of log entry possible
-#[derive(PartialEq, Eq, Hash, Debug)]
-enum EntryType {
+#[derive(PartialEq, Eq, Hash, PartialOrd, Ord, Debug)]
+enum EventType {
     /// Guard n started his shift
     ShiftStart(usize),
     /// Current guard went to sleep
@@ -69,15 +70,15 @@ enum EntryType {
     Wake,
 }
 
-impl FromStr for EntryType {
+impl FromStr for EventType {
     /// eg. [1518-11-01 00:00] Guard #10 begins shift
     /// [1518-11-01 00:05] falls asleep
     /// [1518-11-01 00:25] wakes up
     type Err = String;
 
-    fn from_str(s: &str) -> Result<EntryType, Self::Err> {
+    fn from_str(s: &str) -> Result<EventType, Self::Err> {
         // We'll assume that the [date-part] is gone and we're left with everything after "] "
-        use EntryType::*;
+        use EventType::*;
         if s.starts_with("Guard #") {
             s.splitn(2, '#')
                 .nth(1)
@@ -98,11 +99,78 @@ impl FromStr for EntryType {
 
 #[test]
 fn test_entry_type_parse() {
-    let shift10: EntryType = "Guard #10 begins shift".parse().unwrap();
-    let sleep: EntryType = "falls asleep".parse().unwrap();
-    let wake: EntryType = "wakes up".parse().unwrap();
-    use EntryType::*;
+    let shift10: EventType = "Guard #10 begins shift".parse().unwrap();
+    let sleep: EventType = "falls asleep".parse().unwrap();
+    let wake: EventType = "wakes up".parse().unwrap();
+    use EventType::*;
     assert_eq!(shift10, ShiftStart(10));
     assert_eq!(sleep, Sleep);
     assert_eq!(wake, Wake);
+}
+
+#[derive(PartialEq, Eq, Hash, PartialOrd, Ord, Debug)]
+struct LogEntry {
+    minute: Minute,
+    event: EventType,
+}
+
+impl FromStr for LogEntry {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<LogEntry, String> {
+        let parts: Vec<&str> = s.splitn(2, ']').collect();
+        assert_eq!(parts.len(), 2);
+        let (minute, event) = (parts[0], parts[1]);
+        let minute: Minute = minute.parse().map_err(|e: ParseIntError| {
+            format!("Unable to parse minute: error: {} - input: {}", e, s)
+        })?;
+        let event: EventType = event.trim().parse()?;
+        Ok(LogEntry { minute, event })
+    }
+}
+
+#[test]
+fn test_parse_log_entry() {
+    let shift10: LogEntry = "[1518-11-01 00:00] Guard #10 begins shift".parse().unwrap();
+    let sleep: LogEntry = "[1518-11-01 00:05] falls asleep".parse().unwrap();
+    let wake: LogEntry = "[1518-11-01 00:25] wakes up".parse().unwrap();
+    assert_eq!(
+        shift10,
+        LogEntry {
+            minute: Minute {
+                y: 1518,
+                m: 11,
+                d: 1,
+                h: 0,
+                n: 0
+            },
+            event: EventType::ShiftStart(10),
+        }
+    );
+    assert_eq!(
+        sleep,
+        LogEntry {
+            minute: Minute {
+                y: 1518,
+                m: 11,
+                d: 1,
+                h: 0,
+                n: 5
+            },
+            event: EventType::Sleep,
+        }
+    );
+    assert_eq!(
+        wake,
+        LogEntry {
+            minute: Minute {
+                y: 1518,
+                m: 11,
+                d: 1,
+                h: 0,
+                n: 25,
+            },
+            event: EventType::Wake,
+        }
+    );
 }
